c1420e9ac62014ec4246ec38dea47f59b4650214
Kyusik Shin <sks156324@gmail.com>
2016-02-15 18:33:12 +0900

    add process information. Next, I have to configure UI

--- monitor/views.py
+++ monitor/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from models import Monitor
 from django.views.generic.base import TemplateView
-import psutil
+import psutil, datetime
 
 class SyetmeInfomation:
     
@@ -28,7 +28,7 @@ class SyetmeInfomation:
         pInfo = []
         for proc in psutil.process_iter():                              
             memory = []
-            memory.append('{:,}'.format(proc.memory_info().rss / self.mega) + " M (" + "%05.2f" % proc.memory_percent() + ")")
+            memory.append('{:,}'.format(proc.memory_info().rss / self.mega) + " M (" + "%05.2f" % proc.memory_percent() + "%)")
             memory.append('{:,}'.format(proc.memory_info().vms / self.mega) + " M")                    
             pInfo.append([proc.name(), proc.cpu_percent(), memory, proc.pid, proc.memory_info().rss] )
                     
@@ -75,8 +75,35 @@ class MonitorView(TemplateView):
 class ProcessView(TemplateView):
     
     def get(self, request, pid):
-        proc = psutil.Process(int(pid))
-        output = { 'name' : proc.name() ,
+        proc = psutil.Process(int(pid))        
+        output = {'name'    : proc.name(),
+                  'pid'     : proc.pid,
+                  'ppid'    : proc.ppid(),
+                  'exe'     : proc.exe(),
+                  'cmdline' : proc.cmdline(),
+                  'created' : datetime.datetime.fromtimestamp(proc.create_time()).strftime("%Y-%m-%d %H:%M:%S"),
+                  'status'  : proc.status(),
+                  'cwd'     : proc.cwd(),
+                  'username': proc.username(),
+                  'uids'    : proc.uids(),
+                  'gids'    : proc.gids(),
+                  'nice'    : proc.nice(),
+                  'ionice'  : proc.ionice(),
+                  'rlimit'  : proc.rlimit(psutil.RLIMIT_FSIZE),
+                  'io_counters' : proc.io_counters(),
+                  'num_ctx_switches' : proc.num_ctx_switches(),
+                  'num_fds' : proc.num_fds(),
+                  'num_threads' : proc.num_threads(),
+                  'threads' : proc.threads(),                  
+                  'cpu_times' : proc.cpu_times(),
+#                   'cpu_percent' : proc.cpu_percent(),
+                  'cpu_affinity' : proc.cpu_affinity(),
+                  'memory_info_ex' : proc.memory_info_ex(),
+                  'memory_percent' : proc.memory_percent(),
+                  'children' : proc.children(),              
+                  'open_files' : proc.open_files(),     
+                  'connections' : proc.connections(),
+                  'is_running' : proc.is_running(),             
                   
                   }
         return render(request, 'monitor/process.html', output)
