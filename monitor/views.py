9f47423d41b32b80191eeca142b9fe5732bf91a6
Kyusik Shin <sks156324@gmail.com>
2016-02-17 20:23:58 +0900

    have to make update thread for giving to user concurrent view

--- monitor/views.py
+++ monitor/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from models import Monitor
 from django.views.generic.base import TemplateView
 import psutil, datetime
+import threading, time
+
+iii = 0
+
+def loop():
+    global iii
+    while True:
+        iii += 1
+        time.sleep(1)
+
+th = threading.Thread(target=loop, args=())
+th.start()
+
+refreshInterval = 1000
 
 class SyetmeInfomation:
     
@@ -11,9 +25,17 @@ class SyetmeInfomation:
     
     def __init__(self):
         self.name = 'test'
-        self.processes = []
+        self.allProcesses = []
+        self.processesByMemory = []
+        self.processesByCpu = []
         self.cpus = []
-        self.memory = {}        
+        self.memory = {}
+        
+    def byName(self, item):
+        return item['name']
+    
+    def byPID(self, item):
+        return item['pid']            
         
     def byCpu(self, item):
         return item['cpu_percent']
@@ -27,8 +49,10 @@ class SyetmeInfomation:
     def processUpdate(self):
         pInfo = []
         for proc in psutil.process_iter():            
-            memory = {'rss' : '{:,}'.format(proc.memory_info().rss / self.mega),
-                      'vms' : '{:,}'.format(proc.memory_info().vms / self.mega), 
+            memory = {'rss' : '{:,}'.format(proc.memory_info().rss),
+                      'vms' : '{:,}'.format(proc.memory_info().vms),
+                      'rssM' : '{:,}'.format(proc.memory_info().rss / self.mega),
+                      'vmsM' : '{:,}'.format(proc.memory_info().vms / self.mega),
                       }
             proc = {'name' : proc.name(),
                     'pid' : proc.pid, 
@@ -38,14 +62,22 @@ class SyetmeInfomation:
                     }                   
             pInfo.append(proc)
                     
-                                
-        self.processes = pInfo
-        self.processes.sort(key=self.byRam, reverse=True)
-        self.processes = self.processes[:5] 
+        
+        pInfo.sort(key=self.byName, reverse=False)
+        pInfo.sort(key=self.byPID, reverse=True)                        
+        self.allProcesses = pInfo[:]
+        
+        pInfo.sort(key=self.byCpu, reverse=True)
+        self.processesByCpu = pInfo[:]
+        self.processesByCpu = self.processesByCpu[:5]
+        
+        pInfo.sort(key=self.byRam, reverse=True)
+        self.processesByMemory = pInfo[:] 
+        self.processesByMemory = self.processesByMemory[:5]         
         
     def cpuUpdate(self):
-        self.cpus = []
-        cores = psutil.cpu_percent(percpu=True)
+        self.cpus = []        
+        cores = psutil.cpu_percent(percpu=True)        
         for core in range(len(cores)):
             self.cpus.append( {'numOfCore' : core, 
                                'clock' :cores[core],
@@ -67,17 +99,20 @@ class SyetmeInfomation:
         
     def __del__(self):
         pass
+    
         
 
 class MonitorView(TemplateView):        
-    
     def get(self, request):
         sysinfo = SyetmeInfomation()                
         sysinfo.update() 
-               
-        data = { 'processes' : sysinfo.processes,
-                 'cpus' : sysinfo.cpus,          
-                 'memory' : sysinfo.memory,              
+        print (iii)
+        data = {'processes' : sysinfo.allProcesses,
+                'processesByCpu' : sysinfo.processesByCpu,
+                'processesByMemory' : sysinfo.processesByMemory,
+                'cpus' : sysinfo.cpus,          
+                'memory' : sysinfo.memory,
+                'refreshInterval': 2000,              
                  }
         
         sysinfo.__del__()
@@ -87,9 +122,23 @@ class MonitorView(TemplateView):
 class ProcessView(TemplateView):
     
     def get(self, request, pid):
-        proc = psutil.Process(int(pid))        
+        proc = psutil.Process(int(pid))
+        parentName = "None"
+        cpu_percent = 0.0
+        
+        for p in psutil.process_iter():
+            if int(p.pid) == int(pid):                
+                cpu_percent = p.cpu_percent()
+                break
+        
+        #This process is first process after system boot. There is No parent process
+        if int(proc.ppid()) != 0:
+            parentProcess = psutil.Process(int(proc.ppid()))
+            parentName = parentProcess.name()
+                     
         output = {'name'    : proc.name(),
                   'pid'     : proc.pid,
+                  'parent_name' : parentName,
                   'ppid'    : proc.ppid(),
                   'exe'     : proc.exe(),
                   'cmdline' : proc.cmdline(),
@@ -108,14 +157,15 @@ class ProcessView(TemplateView):
                   'num_threads' : proc.num_threads(),
                   'threads' : proc.threads(),                  
                   'cpu_times' : proc.cpu_times(),
-#                   'cpu_percent' : proc.cpu_percent(),
+                  'cpu_percent' : cpu_percent,
                   'cpu_affinity' : proc.cpu_affinity(),
                   'memory_info_ex' : proc.memory_info_ex(),
                   'memory_percent' : proc.memory_percent(),
                   'children' : proc.children(),              
                   'open_files' : proc.open_files(),     
                   'connections' : proc.connections(),
-                  'is_running' : proc.is_running(),             
+                  'is_running' : proc.is_running(), 
+                  'refreshInterval' : 100000,            
                   
                   }
         return render(request, 'monitor/process.html', output)
